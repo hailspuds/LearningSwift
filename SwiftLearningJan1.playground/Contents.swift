2321ce94f15d3b1edc45d94386e7b6d1d5904f62
Will <code@willo.info>
2016-01-01 19:12:54 +1100

    day 1

---
+++ SwiftLearningJan1.playground/Contents.swift
@@ -0,0 +1,47 @@
+//Jan 1 quick intro tutorial from https://www.hackingwithswift.com
+
+
+import UIKit
+
+var name: String
+
+name = "John Smith"
+
+name = "Romeo"
+
+
+var age: Int
+age = 20
+
+
+var lat: Double
+var long: Float
+lat = 93939393.39393939
+long = 292.3
+
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate
+
+
+"Your name is \(name)" + " and your age is \(age)"
+"in 20 years you'll be \(age + 20)"
+
+var songs = ["Shake it Off", "You Belong with Me", "Love Story"]
+var songs2 = ["Today was a Fairytale", "White Horse", "Fifteen"]
+
+var both = songs + songs2
+
+both += ["Trouble"]
+
+//dictionaries
+
+
+
+
+
+
+
+
+

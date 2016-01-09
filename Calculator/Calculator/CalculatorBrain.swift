27d08cce8edea282daf0d9e44e20e5b2aa4d6969
Will <code@willo.info>
2016-01-09 13:08:04 +1100

    day 9
    calc tute continues and it’s getting really confusing. will a) have to
    go and read more and b) reduce the complexity.
    
    https://www.youtube.com/watch?v=OJUsghbtbF8
    up to 1:07:40

---
+++ Calculator/Calculator/CalculatorBrain.swift
@@ -0,0 +1,88 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Will Ockenden on 8/01/2016.
+//  Copyright © 2016 Hailspuds. All rights reserved.
+//
+    //https://www.youtube.com/watch?v=OJUsghbtbF8
+    //24:20 in
+    //https://itunes.apple.com/us/course/developing-ios-8-apps-swift/id961180099
+    //https://medium.com/@samvlu/100-days-of-swift-736d45a19b63#.vr1m47wtf
+    //http://www.raywenderlich.com/rwpodcast/page/5
+    //http://www.howtogeek.com/230225/how-to-sideload-apps-onto-an-iphone-or-ipad-without-jailbreaking/
+
+
+import Foundation
+
+class CalculatorBrain {
+    
+    private enum Op {
+        //one thing one time, one thing another time
+        //https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Enumerations.html
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+    }
+    
+    //alternate syntax to declare array. same as Array<Op>
+    private var opStack = [Op]()
+    
+    private var knownOps = Dictionary<String, Op>()    //=[String:Op]()
+    
+    init() {
+        knownOps["×"] = Op.BinaryOperation("×", *)
+        knownOps["÷"] = Op.BinaryOperation("÷") { $1 / $0 }
+        knownOps["+"] = Op.BinaryOperation("+", +)
+        knownOps["−"] = Op.BinaryOperation("−") { $1 - $0 }
+        knownOps["√"] = Op.UnaryOperation("√", sqrt)
+    }
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+                
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+                    
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(op1Evaluation.remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        print("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    
+    func pushOperand(operand: Double) -> Double? {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation(symbol: String) -> Double? {
+        if let operation = knownOps[symbol] {
+            opStack.append(operation)
+        }
+        return evaluate()
+    }
+    
+    
+}

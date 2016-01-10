94f8498ecbe3a104005e4ddbb18862797303fc32
Will <code@willo.info>
2016-01-10 16:45:32 +1100

    ADDED: Project2 Flag Game

---
+++ Project2_FlagGame/Project2_FlagGame/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Project2_FlagGame
+//
+//  Created by Will Ockenden on 10/01/2016.
+//  Copyright © 2016 Sill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+
+}
+

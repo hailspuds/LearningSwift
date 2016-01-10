086c255240060d23a4f5a4308dcad529c02f4535
Will <code@willo.info>
2016-01-10 17:28:03 +1100

    Updated: Flag Game

--- Project2_FlagGame/Project2_FlagGame/ViewController.swift
+++ Project2_FlagGame/Project2_FlagGame/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //        countries.append("estonia")
+        //        countries.append("france")
+        //        countries.append("germany")
+        //        countries.append("ireland")
+        //        countries.append("italy")
+        //        countries.append("monaco")
+        //        countries.append("nigeria")
+        //        countries.append("poland")
+        //        countries.append("russia")
+        //        countries.append("spain")
+        //        countries.append("uk")
+        //        countries.append("us")
+        
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        //styling
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        button1.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button2.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button3.layer.borderColor = UIColor.lightGrayColor().CGColor
+        
+        
+        askQuestion(nil)
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +52,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
+    
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    
+    func askQuestion(action: UIAlertAction!) {
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        title = countries[correctAnswer].uppercaseString
+    }
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        var title: String
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            ++score
+        } else {
+            title = "Wrong"
+            --score
+        }
+        
+        //UIAlertActionStyle
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: askQuestion))
+        presentViewController(ac, animated: true, completion: nil)
+ 
+    }
+    
 
 }
 

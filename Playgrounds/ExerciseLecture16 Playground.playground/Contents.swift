cbf8e2f856682faa4d8f2615c0a2d7a1fdfddd3d
Will <code@willo.info>
2016-01-11 17:11:37 +1100

    Added Exercise Lecture 16, Section 2

---
+++ Playgrounds/ExerciseLecture16 Playground.playground/Contents.swift
@@ -0,0 +1,62 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Create a variable with a name of your choosing that has a value of 15
+
+var intVar1: Int = 15
+
+//Create a 2nd variable with a name of your choosing that has a value of 5
+
+var intVar2: Int = 5
+
+//Create a 3rd variable that stores that sum of the 1st and 2nd variables (20 should print to the screen)
+
+var intSumPlus1 = intVar1 + intVar2
+
+//Create a 4th variable that stores the result of a multiplication operation - the stored result should be equal to 45
+
+var intMultiply1 = 45 * 1
+
+//Create a 5th variable that stores the result of the 1st variable minus the 2nd variable - the result should be 10
+
+var intSum2 = intVar1 - intVar2
+
+//Create a 6th variable that stores the result of the 1st variable divided by the 2nd variable - the result should be 3
+
+var intSum3 = intVar1 / intVar2
+
+//Create 3 new variables of any name and of any value, but all on the same line
+
+var threeVars1 = 1, threeVars2 = 2, threeVars3 = 3
+
+//Create 3 constants (let) of any name and of any value, but all on the same line
+
+let threeVars1Constant = 1, threeVars2Constant = 2, threeVars3Constant = 3
+
+//Create an explicit variable of any name of type Double and don't give it any value
+
+var explicitVar:Double
+
+//Create another explicit variable of any name of type Int but DO give it an initial value of 423
+
+var explicitVar2:Int = 423
+
+//Create a variable of type String with a value of "Hello"
+
+var stringVar1 = "Hello"
+
+//Create another variable of type String with a value of "World"
+
+var stringVar2 = "World"
+
+//Create a 3rd variable to store the result of concatenating the first two strings (using the + symbol)
+
+var stringString = stringVar1 + stringVar2
+
+
+//Using that 3rd variable again, store the result of concatenating the first two string but instead using "\(someVar)" syntax
+
+var stringString2 = "\(stringVar1) \(stringVar2)"

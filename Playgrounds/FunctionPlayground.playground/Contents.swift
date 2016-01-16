1ef835cfb3f9680d008f9e206651b5558f66cfec
Will <code@willo.info>
2016-01-16 11:55:59 +1100

    Function exercise playground

---
+++ Playgrounds/FunctionPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func add(param1: Double, param2: Double) -> Double {
+    return param1 + param2
+}
+
+
+add(2, param2: 2)
+
+
+
+func subtract(param1: Int, param2: Int) -> Int {
+    return param1 - param2
+}
+
+subtract(2, param2: 1)
+
+func multiply(param1: Float, param2: Float) -> Float {
+    return param1 * param2
+}
+
+multiply(2, param2: 10)
+
+
+func divide(param1: Double, param2: Double) -> Double {
+    return param1 / param2
+}
+
+divide(10, param2: 2)
+
+
+//easy

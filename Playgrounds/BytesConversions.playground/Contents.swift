565c34cbdb9a0626a8ed3f5e139fb0ae8d96ec4d
Will <code@willo.info>
2016-01-17 18:22:39 +1100

    Bytes Conversion project

---
+++ Playgrounds/BytesConversions.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var quota: Double = 200000000000
+var usage: Double = 22673794313
+
+print ("Quota bytes = \(quota)")
+print ("Quota gigabytes = \(quota / 1000 / 1000) GB")
+print ("Usage gigabytes = \(usage / 1000 / 1000) GB")
+
